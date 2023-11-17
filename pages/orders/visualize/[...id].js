95ce57b2ff522b45e8f2709ed9370420e3cca187
Pab Her <peh_tj@hotmail.com>
2023-11-17 09:24:04 -0300

    Orders Date Correction View

--- pages/orders/visualize/[...id].js
+++ pages/orders/visualize/[...id].js
@@ -169,7 +169,13 @@ export default function VisualizeOrderPage() {
                     Fecha:
                   </h4>
                   <span className="text-normal text-xs md:text-sm">
-                    {new Date(order.dateOrder).toLocaleDateString()}
+                    {new Date(order.dateOrder)
+                      .toISOString()
+                      .slice(0, 10)
+                      .match(/([^T]+)/)[0]
+                      .split("-")
+                      .reverse()
+                      .join("/")}
                   </span>
                 </div>
                 <div className="flex items-center gap-3">
@@ -274,7 +280,7 @@ export default function VisualizeOrderPage() {
                   >
                     <path
                       d="M480 257.35c0-123.7-100.3-224-224-224s-224 100.3-224 224c0 111.8 81.9 204.47 189 221.29V322.12h-56.89v-64.77H221V208c0-56.13 33.45-87.16 84.61-87.16 24.51 0 50.15 4.38 50.15 4.38v55.13H327.5c-27.81 0-36.51 17.26-36.51 35v42h62.12l-9.92 64.77H291v156.54c107.1-16.81 189-109.48 189-221.31z"
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                     />
                   </svg>
                   <span className=" flex items-center justify-center text-normal text-[9px] text-gray-700">
